e67dd583e17d6424c3ad3fa874e7d76804a61e11
Graham Ollis <plicease@cpan.org>
2019-09-18 03:33:32 -0600

    fix it so it works

--- lib/FFI/Platypus/Record/StringArray.pm
+++ lib/FFI/Platypus/Record/StringArray.pm
@@ -3,7 +3,7 @@ package FFI::Platypus::Record::StringArray;
 use strict;
 use warnings;
 use 5.008001;
-use FFI::Platypus 0.96;
+use FFI::Platypus::Memory qw( strdup calloc free );
 use constant _ptr_size => FFI::Platypus->new->sizeof('opaque');
 
 # ABSTRACT: Array of strings for your FFI record
@@ -35,16 +35,19 @@ sub new
 {
   my $class = shift;
 
-  $ffi ||= FFI::Platypus->new;
+  $ffi ||= FFI::Platypus->new( lib => [undef] );
 
-  my $perl_array = [map { defined $_ ? "$_" : undef } @_];
-  my $array_size = _ptr_size * @_;
-  my $type       = "record($array_size)";
+  my $size       = @_;
+  my $array      = [map { defined $_ ? strdup($_) : undef } @_];
+  my $opaque     = calloc($size, _ptr_size);
+
+  $ffi->function(
+    memcpy => [ 'opaque', "opaque[$size]", 'size_t' ] => 'opaque',
+  )->call($opaque, $array, $size * _ptr_size);
 
   my $self = bless {
-    perl_array => $perl_array,
-    c_array    => $ffi->cast( 'string[]' => $type, $perl_array),
-    type       => $type,
+    array => $array,
+    opaque => $opaque,
   }, $class;
 }
 
@@ -61,8 +64,7 @@ Returns the opaque pointer to the array of C strings.
 sub opaque
 {
   my($self) = @_;
-  my $type = $self->{type};
-  $self->{opaque} ||= $ffi->cast( $type => 'opaque', $self->{c_array});
+  $self->{opaque};
 }
 
 =head2 size
@@ -76,7 +78,7 @@ Returns the number of elements in the array of C strings.
 sub size
 {
   my($self) = @_;
-  scalar @{ $self->{perl_array} };
+  scalar @{ $self->{array} };
 }
 
 =head2 element
@@ -90,7 +92,14 @@ Returns the string in the array of C strings at the given index.
 sub element
 {
   my($self, $index) = @_;
-  $self->{perl_array}->[$index];
+  $ffi->cast( 'opaque' => 'string', $self->{array}->[$index] );
+}
+
+sub DESTROY
+{
+  my($self) = @_;
+  free($_) for grep { defined $_ } @{ $self->{array} };
+  free($self->{opaque});
 }
 
 1;
